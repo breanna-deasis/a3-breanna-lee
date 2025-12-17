09bdb91b7bf107c3be2abc40b2aac5998ab7309b
bree <blee1@wpi.edu>
2025-12-17 14:30:40 -0800

    add required path

--- db.js
+++ db.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI)
-        console.log('MongoDB Connected: ${conn.connection.host}');
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
 
         mongoose.connection.on('error', err => {
-            console.error('MongoDB connection error: ${err}');
+            console.error(`MongoDB connection error: ${err}`);
         })
 
         mongoose.connection.on('disconnected', () => {
